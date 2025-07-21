f1069cd9479f8af8ddf4ad5405433ae798cbc134
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-07-21 21:19:35 +0300

    jwt for photo api added

--- Services/PhotoStock/OnlineCourse.Services.PhotoStock/Startup.cs
+++ Services/PhotoStock/OnlineCourse.Services.PhotoStock/Startup.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,7 +28,18 @@ namespace OnlineCourse.Services.PhotoStock
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_catalog";
+                options.RequireHttpsMetadata = false;
+            });
+
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter());
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OnlineCourse.Services.PhotoStock", Version = "v1" });
@@ -48,6 +61,7 @@ namespace OnlineCourse.Services.PhotoStock
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
