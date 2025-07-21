d90453b096543f92faa20ff4580a345ee1cc117f
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-07-21 17:46:23 +0300

    User controller created, config file updated on identity server

---
+++ IdentityServer/OnlineCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnlineCourse.IdentityServer.Dtos;
+using OnlineCourse.IdentityServer.Models;
+using OnlineCourse.Shared.Dtos;
+
+namespace OnlineCourse.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUp)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUp.UserName,
+                Email = signUp.Email,
+                City = signUp.City
+            };
+
+            var result = await _userManager.CreateAsync(user,signUp.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 404));
+            }
+
+            return NoContent();
+        }
+    }
+}
