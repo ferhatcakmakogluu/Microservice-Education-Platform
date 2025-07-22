fba935fb37b26e3df22fa16f2f8f6f2851815315
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-07-23 00:23:19 +0300

    basket startup update -> basket api protected

--- Services/Basket/OnlineCourse.Services.Basket/Startup.cs
+++ Services/Basket/OnlineCourse.Services.Basket/Startup.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,13 +32,22 @@ namespace OnlineCourse.Services.Basket
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["IdentityServerURL"];
+                options.Audience = "resource_catalog";
+                options.RequireHttpsMetadata = false;
+            });
+
+
             services.AddHttpContextAccessor();
 
             services.AddScoped<ISharedIdentityService, SharedIdentityService>();
             services.AddScoped<IBasketService, BasketService>();
 
             //redis
-
             services.Configure<RedisSettings>(Configuration.GetSection("RedisSettings"));
 
 
@@ -50,7 +62,10 @@ namespace OnlineCourse.Services.Basket
                 return redis;
             });
 
-            services.AddControllers();
+            services.AddControllers(opt =>
+            {
+                opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OnlineCourse.Services.Basket", Version = "v1" });
@@ -70,7 +85,7 @@ namespace OnlineCourse.Services.Basket
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseAuthentication();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
