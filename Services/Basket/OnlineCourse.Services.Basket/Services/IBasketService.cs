a5709025e667df67533a50a22a3d78c55e86acb6
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-07-22 23:34:24 +0300

    redis configurations edited, IBasketService created

---
+++ Services/Basket/OnlineCourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using OnlineCourse.Services.Basket.Dtos;
+using OnlineCourse.Shared.Dtos;
+using System.Threading.Tasks;
+
+namespace OnlineCourse.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basket);
+        Task<Response<bool>> Delete(string userId);
+    }
+}
