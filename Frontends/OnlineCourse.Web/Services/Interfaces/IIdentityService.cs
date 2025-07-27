55722862cd5fb4f1ed393b04b1c88f2da57a2aa0
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-07-27 23:09:17 +0300

    update bootstrap to v5.0, indetity service interface created

---
+++ Frontends/OnlineCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication.OAuth;
+using OnlineCourse.Shared.Dtos;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using OnlineCourse.Web.Models;
+
+namespace OnlineCourse.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SignInInput signInInput);
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+    }
+}
