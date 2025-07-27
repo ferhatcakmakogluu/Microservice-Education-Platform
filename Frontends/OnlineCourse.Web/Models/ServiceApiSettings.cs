d9c3622eed5d35279afa62304e1741f2a973d775
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-07-27 23:13:42 +0300

    service api service entity created and configured

---
+++ Frontends/OnlineCourse.Web/Models/ServiceApiSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineCourse.Web.Models
+{
+    public class ServiceApiSettings
+    {
+        public string BaseUrl { get; set; }
+        public string PhotoStockUrl { get; set; }
+    }
+}
--- Frontends/OnlineCourse.Web/Startup.cs
+++ Frontends/OnlineCourse.Web/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OnlineCourse.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,7 @@ namespace OnlineCourse.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ServiceApiSettings>(Configuration.GetSection("ServiceApiSettings"));
             services.AddControllersWithViews();
         }
 
