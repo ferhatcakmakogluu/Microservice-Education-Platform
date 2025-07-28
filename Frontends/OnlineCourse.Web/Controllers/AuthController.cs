5d0189b0bcec0f876828020ce2e24b068cf1b92e
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-07-28 20:02:03 +0300

    Auth controller and SignIn method created in mvc layer

---
+++ Frontends/OnlineCourse.Web/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using OnlineCourse.Web.Models;
+using OnlineCourse.Web.Services.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace OnlineCourse.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(SignInInput signInInput)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            
+            var response = await _identityService.SignIn(signInInput);
+
+            if (!response.IsSuccessful)
+            {
+                response.Errors.ForEach(x =>
+                {
+                    ModelState.AddModelError(String.Empty, x);
+                });
+                return View();
+            }
+
+            return RedirectToAction(nameof(Index), "Home");
+        }
+    }
+}
--- Frontends/OnlineCourse.Web/Models/SignInInput.cs
+++ Frontends/OnlineCourse.Web/Models/SignInInput.cs
@@ -4,10 +4,14 @@ namespace OnlineCourse.Web.Models
 {
     public class SignInInput
     {
+        [Required]
         [Display(Name ="Email Adresiniz")]
         public string Email { get; set; }
+
+        [Required]
         [Display(Name = "Şifreniz")]
         public string Password { get; set; }
+
         [Display(Name = "Beni Hatırla")]
         public bool IsRemember { get; set; }
     }
