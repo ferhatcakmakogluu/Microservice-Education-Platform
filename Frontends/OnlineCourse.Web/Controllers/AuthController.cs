520f1d137022b0ea113e2af7a929e0b51e94667f
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-07-30 19:51:32 +0300

    layout update, logout func completed

--- Frontends/OnlineCourse.Web/Controllers/AuthController.cs
+++ Frontends/OnlineCourse.Web/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using OnlineCourse.Web.Models;
 using OnlineCourse.Web.Services.Interfaces;
@@ -41,6 +43,14 @@ namespace OnlineCourse.Web.Controllers
             }
 
             return RedirectToAction(nameof(Index), "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await _identityService.RevokeRefreshToken();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
