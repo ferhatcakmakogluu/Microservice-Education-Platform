ff3a90fa7159db8113840ca68e7e3a815965a249
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-07-31 22:08:11 +0300

    course delete process (page and endpoint) completed

--- Frontends/OnlineCourse.Web/Controllers/CoursesController.cs
+++ Frontends/OnlineCourse.Web/Controllers/CoursesController.cs
@@ -91,5 +91,11 @@ namespace OnlineCourse.Web.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _catalogService.DeleteCourseAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
