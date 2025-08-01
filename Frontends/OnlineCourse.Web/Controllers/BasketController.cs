5f096e9da884d8a935b4a08022df356ffe2494c3
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-08-01 16:42:27 +0300

    basket controller and page completed

---
+++ Frontends/OnlineCourse.Web/Controllers/BasketController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineCourse.Web.Models.Baskets;
+using OnlineCourse.Web.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace OnlineCourse.Web.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+
+        public BasketController(ICatalogService catalogService, IBasketService basketService)
+        {
+            _catalogService = catalogService;
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _basketService.Get());
+        }
+
+        public async Task<IActionResult> AddBasketItem(string courseId)
+        {
+            var course = await _catalogService.GetByCourseIdAsync(courseId);
+            var basketItem = new BasketItemViewModel
+            {
+                CourseId = courseId,
+                CourseName = course.Name,
+                Price = course.Price
+            };
+
+            await _basketService.AddBasketItem(basketItem);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> RemoveBasketItem(string courseId)
+        {
+            await _basketService.RemoveBasketItem(courseId);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
