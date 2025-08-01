c88b762b2609471d868e8c99974c84a1568bc23e
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-08-01 16:18:41 +0300

    basket service completed (discount methods not yet)

--- Services/Basket/OnlineCourse.Services.Basket/Controllers/BasketsController.cs
+++ Services/Basket/OnlineCourse.Services.Basket/Controllers/BasketsController.cs
@@ -30,6 +30,7 @@ namespace OnlineCourse.Services.Basket.Controllers
         [HttpPost]
         public async Task<IActionResult> SaveOrUpdateBasket(BasketDto basketDto)
         {
+            basketDto.UserId = _sharedIdentityService.GetUserId;
             var response = await _basketService.SaveOrUpdate(basketDto);
             return CreateActionResultInstance(response);
         }
