96f285e9d0f8c73953d84993e1e726dabd1fecc0
Ferhatcakmakogluu <ferhat.cakmakoglu61@gmail.com>
2025-08-02 11:44:02 +0300

    payment info input model completed

--- Frontends/OnlineCourse.Web/Services/PaymentService.cs
+++ Frontends/OnlineCourse.Web/Services/PaymentService.cs
@@ -1,14 +1,24 @@
 ﻿using OnlineCourse.Web.Models.FakePayments;
 using OnlineCourse.Web.Services.Interfaces;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace OnlineCourse.Web.Services
 {
     public class PaymentService : IPaymentService
     {
-        public Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+        private readonly HttpClient _httpClient;
+
+        public PaymentService(HttpClient httpClient)
         {
-            throw new System.NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<bool> ReceivePayment(PaymentInfoInput paymentInfoInput)
+        {
+            var response = await _httpClient.PostAsJsonAsync<PaymentInfoInput>("fakepayments", paymentInfoInput);
+            return response.IsSuccessStatusCode;
         }
     }
 }
